84f31c94b171002499a2d0ed4c3f722d98fa9f7a
vtrbtf <vitor.tfreitas@gmail.com>
2017-10-23 10:17:53 -0200

    WIP

--- implementation/apple-and-orange/main.py
+++ implementation/apple-and-orange/main.py
@@ -1,9 +1,25 @@
 #!/usr/bin/env python3
- 
-house = input('')
-trees = input('')
-m_and_n = input('')
-m_distances = input('') 
-n_distances = input('') 
+
+house = [int(n) for n in input('input house coords \n').split(' ')]
+trees = [int(n) for n in input('Input trees coords \n').split(' ')]
+m_and_n = [int(n) for n in input('input m and n coords \n').split(' ')]
+m_distances = [int(n) for n in input('input m dist \n').split(' ')]
+n_distances = [int(n) for n in input('input n dist \n').split(' ')]
+
+m_counter = 0
+for md in m_distances:
+    if md > 0:
+        ground_dist = trees[0] + md
+        if ground_dist >= house[0] and ground_dist <= house[1]:
+            m_counter += 1
 
 
+n_counter = 0
+for nd in n_distances:
+    if nd < 0:
+        ground_dist = trees[1] + nd
+        if ground_dist >= house[0] and ground_dist <= house[1]:
+            n_counter += 1
+
+print(m_counter)
+print(n_counter)
